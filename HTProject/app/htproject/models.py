16b12b00b1f2dce9bbfee31809d8451ce3aa8b39
NguyenThanh55 <2051052120thanh@ou.edu.vn>
2023-11-04 15:02:58 +0700

    create admin

---
+++ HTProject/app/htproject/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class BaseModel(models.Model):
+    created_date = models.DateField(auto_now_add=True, null=True)
+    updated_date = models.DateField(auto_now=True, null=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(BaseModel):
+    subject = models.CharField(max_length=100, null=False)
+    description = models.TextField()
+    image = models.ImageField(upload_to='image/%Y/%m')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CourseApp
+
+# Cai dat requirement
+
+- Tạo: pip freeze > requirements.txt
+- Cài đặt thư viện: pip install -r requirements.txt
